f625d750e5eb38fd7a18ce78a73add2e2d593494
Grego Corpas <gregocorpas89@yahoo.es>
2015-09-29 17:33:29 +0200

    Inicializando clase padre

--- tema7/practica4/clases.py
+++ tema7/practica4/clases.py
@@ -1,9 +1,11 @@
-class Publicacion():
+class Publicacion(object):
 	def __init__(self,editor,direccion,pais,isbn,anyo,paginas,tapa):
 		self.editor=editor
 		self.pais=pais
 		self.isbn=isbn
 		self.anyo=anyo
+		self.tapa=tapa
+		self.paginas=paginas
 	def getEditor(self):
 		return self.editor
 	def getPais(self):
@@ -12,38 +14,36 @@ class Publicacion():
 		return self.isbn
 	def getAnyo(self):
 		return self.anyo
-	def getDatos(self):
-		print "Editorial: ",self.editor,"\nPais: ",self.pais,"\nISBN: ",self.isbn,"\nAnyo: ",self.anyio,"\nTapa: ",self.tapa," \nNo de Paginas: ",paginas
+	def getPublicacion(self):
+		print "Editorial: ",self.editor,"\nPais: ",self.pais,"\nISBN: ",self.isbn,"\nAnyo: ",self.anyo,"\nTapa: ",self.tapa," \nNo de Paginas: ",self.paginas
 
 class Libro(Publicacion):
-	def __init__(self,autor,tipo,pais,idioma):
+	def __init__(self,editor,direccion,pais,isbn,anyo,paginas,tapa,autor,tipo,idioma):
 		self.autor=autor
 		self.tipo=tipo
-		self.pais=pais
 		self.idioma=idioma
-
+		Publicacion.__init__(self,editor,direccion,pais,isbn,anyo,paginas,tapa)
 	def getAutor(self):
 		return self.autor
 	def getTipo(self):
 		return self.tipo
-	def getPais(self):
-		return self.pais
 	def getIdioma(self):
 		return self.idioma
+	def getLibro(self):
+		print "Autor: ",self.autor,"\nTipo: ",self.tipo,"\nIdioma: ",self.idioma
 	
 class Comic(Publicacion):
-	def __init__(self,autor,coleccion,pais,idioma):
+	def __init__(self,editor,direccion,pais,isbn,anyo,paginas,tapa,autor,coleccion,idioma):
 		self.autor=autor
 		self.coleccion=coleccion
-		self.pais=pais
 		self.idioma=idioma
-#preguntar como inicializar al padre super
+		Publicacion.__init__(self,editor,direccion,pais,isbn,anyo,paginas,tapa)
 	def getAutor(self):
 		return self.autor
 	def getColeccion(self):
 		return self.coleccion
-	def getPais(self):
-		return self.pais
 	def getIdioma(self):
 		return self.idioma
+	def getComic(self):
+		print "Autor: ",self.autor,"\nColeccion: ",self.coleccion,"\nIdioma: ",self.idioma
 
